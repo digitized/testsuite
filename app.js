7e7f75480a3589d1a17166719e44e01efc12bffa
Alex <alex.lee3851@gmail.com>
2016-04-25 11:06:16 -0700

    grab library name from string

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname+'/code/'+config.folder));
-if( config.library )
-  app.use('/lib', express.static(__dirname+'/code/'+config.library));
+if( config.library ){
+  var libStr = config.library.split('/');
+  var libName = ( libStr[libStr.length-1] === '') ? libStr[libStr.length-2]:libStr[libStr.length-1];
+  app.use('/'+libName, express.static(__dirname+'/code/'+config.library));
+}
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '/code/'+config.testPage));
